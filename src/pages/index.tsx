0700cc25bdb9ccc1832be748a6d8b4f752fa7466
Felipe Ribeiro <piporibeiro@gmail.com>
2021-04-06 23:59:50 -0300

    feat: create header, sidebar and dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,28 @@
-export default function Home() {
-  return <h1>Dashgo</h1>;
+import { Flex, Button, Stack } from '@chakra-ui/react';
+import React from 'react';
+import { Input } from '../components/Form/Input';
+
+export default function SignIn() {
+  return (
+    <Flex w={'100vw'} h={'100vh'} align="center" justify="center">
+      <Flex
+        as={'form'}
+        width="100%"
+        maxWidth={360}
+        bg={'gray.800'}
+        p={8}
+        borderRadius={8}
+        flexDir={'column'}
+      >
+        <Stack spacing={4}>
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
+
+        <Button type="submit" mt={6} colorScheme={'pink'} size={'lg'}>
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
